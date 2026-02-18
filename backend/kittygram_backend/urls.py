a8e64bfe72f47f87f286d2dee781e823a26a2959
VladislavDF <vladov.00@yandex.ru>
2026-02-18 23:50:33 +0300

    Fix

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
-
 from cats.views import AchievementViewSet, CatViewSet
 
 
