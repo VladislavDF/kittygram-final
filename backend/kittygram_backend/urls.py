85f58b36ccf1e0ab247dab88e8a510e3f19c6e6a
VladislavDF <vladov.00@yandex.ru>
2026-02-21 13:03:42 +0300

    Update

---
+++ backend/kittygram_backend/urls.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from cats.views import AchievementViewSet, CatViewSet
+
+
+router = routers.DefaultRouter()
+router.register('cats', CatViewSet)
+router.register('achievements', AchievementViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
+    path('api/auth/token/login/', views.obtain_auth_token),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
