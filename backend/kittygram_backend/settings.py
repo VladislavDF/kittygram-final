3a759fa4d74ffcc48151d7d085cfc5a83e8f9bf5
VladislavDF <vladov.00@yandex.ru>
2026-02-21 14:47:31 +0300

    лишня строка

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-git add backend/kittygram_backend/settings.py
+
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 
