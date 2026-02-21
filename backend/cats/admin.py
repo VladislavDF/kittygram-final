fe471d77454493f662ee34985d1b29100acc96d1
VladislavDF <vladov.00@yandex.ru>
2026-02-21 17:05:22 +0300

    Last fix

---
+++ backend/cats/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Cat, Achievement, AchievementCat
+
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'birth_year', 'owner')
+    list_filter = ('color', 'birth_year')
+    search_fields = ('name', 'owner__username')
+    raw_id_fields = ('owner',)
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(AchievementCat)
+class AchievementCatAdmin(admin.ModelAdmin):
+    list_display = ('achievement', 'cat')
